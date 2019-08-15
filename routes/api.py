cd321899f7d9ce82c0083dca4fd896a78dc04384
Vic Chang <vic.chang@infotimes.com.tw>
2019-08-15 18:47:42 +0800

    Modify api route

--- routes/api.py
+++ routes/api.py
@@ -3,6 +3,6 @@ from api import User, hello
 
 
 # API註冊路由
-#api.add_resource(User.Users, '/users/')
-#api.add_resource(User.User, "/user/<string:name>")
+api.add_resource(User.Users, '/users/')
+api.add_resource(User.User, "/user/<string:name>")
 api.add_resource(hello.helloworld, "/hello/")
